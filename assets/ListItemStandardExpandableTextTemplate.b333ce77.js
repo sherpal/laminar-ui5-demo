4c2d17eb979afce92f79091eb9e1b227f118dfae
sherpal <antoine.doeraene@gmail.com>
2025-08-12 20:22:09 +0000

    See https://github.com/sherpal/LaminarSAPUI5Bindings/commit/fff5498c4ed23a80020c39f81ad0c7b04b31f40f from refs/heads/master

--- assets/ListItemStandardExpandableTextTemplate.b333ce77.js
+++
@@ -1,1 +0,0 @@
-import{j as x,E as m}from"./index.1e3d7808.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
---
+++ assets/ListItemStandardExpandableTextTemplate.ff657f30.js
@@ -0,0 +1,1 @@
+import{j as x,E as m}from"./index.4a093d3d.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
